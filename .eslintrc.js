24a359aba4aee16276571af30c961d21893fa548
dandv <ddascalescu+github@gmail.com>
2017-12-26 19:09:26 -0800

    Upgrade eslint and standardize .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  'parserOptions': {
-    'ecmaVersion': 8,
+  parserOptions: {
+    ecmaVersion: 8,
   },
-  extends: ['airbnb-base'],
-  plugins: [
-    'jest'
-  ],
   env: {
-    'jest/globals': true
+    'jest/globals': true,
   },
+  plugins: [
+    'jest',
+  ],
+  extends: ['airbnb-base'],
   rules: {
     'import/prefer-default-export': 'off',  // https://basarat.gitbooks.io/typescript/content/docs/tips/defaultIsBad.html
-    'no-mixed-operators': 0,
-  }
+    'no-mixed-operators': 'off',
+  },
 };
