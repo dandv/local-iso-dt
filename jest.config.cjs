aef6cd29487bc6be0a376590dee86a7c8c9a4098
Dan Dascalescu <ddascalescu+github@gmail.com>
2020-09-06 17:31:03 -0700

    Remove main: index.mjs from package.json to not confuse Jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '.*.test.ts',  // test only filenames matching this regex
-  moduleFileExtensions: ['ts', 'js'],  // modules are only in .ts files, but 'js' *must* be specified too
 };
